5dd99f98326e38ceb69946346d9b8b83a491cc77
srilakshmijonnala <123008514+srilakshmijonnala@users.noreply.github.com>
2023-11-23 20:12:33 -0600

    update

---
+++ src/main/java/com/example/DMS/repository/trainerRepository.java
@@ -0,0 +1,9 @@
+package com.example.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.DMS.Models.Trainer;
+
+public interface trainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
